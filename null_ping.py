ef46e236d6ba9f1eda8505026ed3a501c0c9aa82
admanne <admanne@gmail.com>
2014-07-21 13:39:42 -0500

    ping

---
+++ null_ping.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+__author__ = 'admanne'
+import sys
+import os
+
+if len(sys.argv) != 4:
+    print "null_ping <subnet> <start> <stop>"
+    sys.exit(0)
+
+subnet = sys.argv[1].strip()
+start = int(sys.argv[2].strip())
+stop = int(sys.argv[3].strip())
+
+print subnet
+
+for x in range(start, stop):
+    addr = str(subnet)+str(x)
+    print addr
+    cmd = 'ping -W 3 -c 1 ' +str(addr)
+    os.system(cmd)

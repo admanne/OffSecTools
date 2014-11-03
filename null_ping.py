67de0967f391c9ff6ff16b3b5f47befbfd013251
admanne <admanne@gmail.com>
2014-11-03 08:51:04 -0600

    post exam
    post exam updates

--- null_ping.py
+++ null_ping.py
@@ -1,18 +1,14 @@
-﻿#!/usr/bin/python
+#!/usr/bin/python
 __author__ = 'admanne'
 import sys
 import os
 
-if len(sys.argv) != 4:
-    print "null_ping <subnet> <start> <stop>"
+if len(sys.argv) != 2:
+    print "null_ping <range>"
+    print "null_ping 192.168.1.1-255"
     sys.exit(0)
 
-subnet = sys.argv[1].strip()
-start = int(sys.argv[2].strip())
-stop = int(sys.argv[3].strip())
+addr = sys.argv[1].strip()
 
-for x in range(start, stop):
-    addr = str(subnet)+str(x)
-    print addr
-    cmd = 'ping -W 3 -c 1 ' +str(addr)
-    os.system(cmd)
+cmd = 'nmap -sP ' +str(addr)
+os.system(cmd)
